325d8c2e61f4b97279039878de6f67d7c01d322f
Lucie Yarish <lucieyarish@hotmail.com>
2024-02-17 08:10:41 +0100

    Create and style main section

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,5 +1,20 @@
+import './Main.css';
+
 const Main = () => {
-  return <h1>Main</h1>;
+  return (
+    <>
+      <main class="main-section">
+        <h1 class="title">Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+    </>
+  );
 };
 
 export default Main;
