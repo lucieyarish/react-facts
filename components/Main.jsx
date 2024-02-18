bb1139eab9e55ee7c076aec743688d2ed4ca59db
Lucie Yarish <lucieyarish@hotmail.com>
2024-02-18 06:25:58 +0100

    Remove redundant fragments

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,18 +2,16 @@ import './Main.css';
 
 const Main = () => {
   return (
-    <>
-      <main class="main-section">
-        <h1 class="title">Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main>
-    </>
+    <main class="main-section">
+      <h1 class="title">Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
   );
 };
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,19 +2,17 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <>
-      <header>
-        <nav className="nav">
-          <img
-            src="/assets/react-icon-small.png"
-            alt="Blue React logo"
-            className="nav--icon"
-          />
-          <h3 className="nav--logo_text">ReactFacts</h3>
-          <h4 className="nav--title">React Course - Project 1</h4>
-        </nav>
-      </header>
-    </>
+    <header>
+      <nav className="nav">
+        <img
+          src="/assets/react-icon-small.png"
+          alt="Blue React logo"
+          className="nav--icon"
+        />
+        <h3 className="nav--logo_text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
+      </nav>
+    </header>
   );
 };
 
